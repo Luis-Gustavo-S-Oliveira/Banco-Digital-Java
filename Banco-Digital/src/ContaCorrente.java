089e80fe355183844a35d49ff4cfdeb01d408de0
Luis-Gustavo-S-Oliveira <luisgu6666@gmail.com>
2025-09-16 21:50:52 -0300

    AjustaContaCorrente

--- Banco-Digital/src/ContaCorrente.java
+++ Banco-Digital/src/ContaCorrente.java
@@ -3,11 +3,12 @@ public class  ContaCorrente extends Conta {
     public ContaCorrente(Cliente cliente) {
         super(cliente);
     }
-
+    Conta conta = new Conta(cliente);
     @Override
     public void exibirExtrato() {
         System.out.println("=== Extrato Conta Corrente ===");
         System.out.println("Cliente: " + cliente.getNome());
+        System.out.println("Agência: " + conta.getAgencia());
         System.out.println("Número da Conta: " + numero);
         System.out.printf("Saldo: R$ %.2f%n", saldo);
         System.out.println("==============================");
